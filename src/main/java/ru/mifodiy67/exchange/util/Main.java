e788e532c0cf29dcc27fec8b75abf4121d08ca14
MikhailovSV <MikhailovSV@sibintek.ru>
2019-10-28 11:22:39 +0300

    second commit: insert data in table

--- src/main/java/ru/mifodiy67/exchange/util/Main.java
+++ src/main/java/ru/mifodiy67/exchange/util/Main.java
@@ -21,21 +21,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        String fileName = "1.txt";
-        String eventName = "ORDER";
-        /*String fileName = "";
+        String fileName = "";
+        String eventName = "";
         if (args.length > 0) {
             fileName = args[0];
-        }*/
+            eventName = args[1];
+        }
 
         ParsingFile service = new ParsingFile();
         List<StatisticInfo> list = service.parsing(fileName);
         Map<Integer, Long> eventStatistics = service.getEventStatistics(list, eventName);
 
         Iterator<Map.Entry<Integer, Long>> iterator = eventStatistics.entrySet().iterator();
-//        Map.Entry<Integer, Long> next = iterator.next();
         int minBorder = 0;
-        int maxBorder;
         int numberOfRecords = list.size();
         long count = 0;
         int firstBorder = 0;
@@ -47,34 +45,36 @@ public class Main {
         float thirdBorderCount = numberOfRecords * THIRD_BORDER_VALUE;
         float fourthBorderCount = numberOfRecords * FOURTH_BORDER_VALUE;
         Set<TotalInfo> totalInfoSet = new TreeSet<>();
+        int countTotalInfo = 0;
         while (iterator.hasNext()) {
             Map.Entry<Integer, Long> next = iterator.next();
             count += next.getValue();
-            maxBorder = next.getKey();
+            countTotalInfo +=next.getValue();
+            int timeValue = next.getKey();
             if (minBorder == 0) {
-                minBorder = maxBorder;
+                minBorder = timeValue;
             }
             if (count >= firstBorderCount && firstBorder == 0) {
-                firstBorder = maxBorder;
+                firstBorder = timeValue;
             } else if (count >= secondBorderCount && secondBorder == 0) {
-                secondBorder = maxBorder;
+                secondBorder = timeValue;
             } else if (count >= thirdBorderCount && thirdBorder == 0) {
-                thirdBorder = maxBorder;
+                thirdBorder = timeValue;
             } else if (count >= fourthBorderCount && fourthBorder == 0) {
-                fourthBorder = maxBorder;
+                fourthBorder = timeValue;
             }
 
-//            if () {
-            TotalInfo totalInfo = new TotalInfo(1, 111, 22.22f, 33.33f);
-            totalInfoSet.add(totalInfo);
-            totalInfo = new TotalInfo(2, 222, 33.33f, 44.44f);
-            totalInfoSet.add(totalInfo);
-//            }
+            if (timeValue % 5 == 0) {
+                float weight = (float) countTotalInfo / numberOfRecords * 100;
+                float percent = (float) count / numberOfRecords * 100;
+                TotalInfo totalInfo = new TotalInfo(timeValue, countTotalInfo, weight, percent);
+                totalInfoSet.add(totalInfo);
+                countTotalInfo = 0;
+            }
         }
         String result = String.format("%s min=%d 50%%=%d 90%%=%d 99%%=%d 99.9%%=%d",
                 eventName, minBorder, firstBorder, secondBorder, thirdBorder, fourthBorder);
         System.out.println(result);
-
 
         OrderDao orderDao = new OrderDao();
         orderDao.saveAll(totalInfoSet);
